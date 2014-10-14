1fbfcea736451f680fc9e18eb6f9fba129604239
Hackbright Student <info@hackbrightacademy.com>
2014-10-13 18:02:52 -0700

    Initial Commit

--- hackbright_app.py
+++ hackbright_app.py
@@ -11,6 +11,13 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" %(first_name. last_name)
+    
+
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
